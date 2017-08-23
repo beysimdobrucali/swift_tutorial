2d33a411e06187c64a108fa5a4395698be6141d1
Besim Dobrucalı <besim.dobrucali@evatro.com>
2017-08-24 00:19:48 +0300

    started a new tutorial

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Besim Dobrucalı on 21.08.2017.
+//  Copyright © 2017 Besim Dobrucalı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
